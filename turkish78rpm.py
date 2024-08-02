f7694b016dfe1b0af91ff08ed958b61c1aff76e2
Mithat Yavuzarslan <68926878+mithatyavuzarslan@users.noreply.github.com>
2024-08-02 11:00:52 -0700

    Update turkish78rpm.py

--- turkish78rpm.py
+++ turkish78rpm.py
@@ -1,10 +1,10 @@
 ##########################
-#  This Python script is to create the dataset of Turkish 78 rpm recordings published between 1905-1965. #
-# The collection is the appendix of the book "Git Zaman Gel Zaman" written by Cemal Unlu. #
-# Selenium package in Python was used to pull data from each page on http://tasplak.pankitap.com where users can access the data contains more than 16000 entries in a JS table #
-# Reference to the book: Ünlü, C. (2016). Git zaman gel zaman: Fonograf-gramofon-taş plak. Pan Yayıncılık. #
-# Reference to the digital appendix: http://tasplak.pankitap.com #
-# Script by: mithatyavuzarslan@gmail.com #
+#  This Python script is to create the dataset of Turkish 78 rpm recordings between the years 1905-1965. 
+# The collection is the appendix of the book "Git Zaman Gel Zaman" written by Cemal Unlu. 
+# Selenium package in Python was used to pull data from each page on http://tasplak.pankitap.com where users can access the data containing more than 16000 entries in a JS table 
+# Reference to the book: Ünlü, C. (2016). Git zaman gel zaman: Fonograf-gramofon-taş plak. Pan Yayıncılık. 
+# Reference to the digital appendix: http://tasplak.pankitap.com 
+# Script by: mithatyavuzarslan@gmail.com 
 ##########################
 
 from selenium import webdriver
@@ -26,7 +26,7 @@ driver.get('http://tasplak.pankitap.com')
 array_records=np.array([])
 
 
-# Define the number of pages to scrape. Someho the page needs to be clicked on the next button twice.
+# Define the number of pages to scrape. Somehow the page needs to be clicked on the next button twice.
 num_pages = 807*2
 
 for i in range(num_pages):
